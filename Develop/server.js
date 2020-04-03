2c951f21d1da5477e02238f1a58e7e72e63fec49
Angel Randall <drivenbysuccess21@gmail.com>
2020-04-02 19:27:19 -0500

    set up express app to handle data parsing

--- Develop/server.js
+++ Develop/server.js
@@ -8,3 +8,7 @@ const app = express();
 
 // Port to Run Heroku localhost
 const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
